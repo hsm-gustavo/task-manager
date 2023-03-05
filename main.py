8ec4605cff39a500d08a3cc87ca131f88a402def
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2023-03-05 18:49:42 -0300

    implemented cli and exception handling

--- main.py
+++ main.py
@@ -29,14 +29,23 @@ class TaskManager:
         cur_task = self.first
         while cur_task is not None:
             if cur_task.name == name:
-                days_left =  datetime.strptime(cur_task.deadline, "%d/%m/%y") - datetime.strptime(self.today, "%d/%m/%y")
+
+                conversion_type = "%d/%m/%y" if len(cur_task.deadline)<9 else "%d/%m/%Y"
+
+                days_left =  datetime.strptime(cur_task.deadline, conversion_type) - datetime.strptime(self.today, "%d/%m/%y")
+
                 return print(f"-------Tarefa encontrada-------\nDescrição: {cur_task.desc}\nPrazo: {cur_task.deadline}\nVocê tem {days_left.days} dia(s) restante(s)") \
                 if days_left.days>=0 else print(f"-------Tarefa encontrada-------\nDescrição: {cur_task.desc}\nPrazo: {cur_task.deadline}\nVocê está {abs(days_left.days)} dia(s) atrasado")
+            
             cur_task = cur_task.next
+            
         print("Tarefa não encontrada")
 
     def remove_task(self, index):
-        if index >= len(self) or index < 0:
+        if len(self) == 0:
+            return print("\nVocê ainda não adicionou nenhuma tarefa\n")
+
+        elif index >= len(self) or index < 0:
             raise IndexError("Index out of range")
         
         if index == 0:
@@ -74,28 +83,55 @@ class TaskManager:
     def is_empty(self) -> bool:
         return self.first == None
     
-    def length(self) -> int:
+    def length(self):
+        return print(f"Você tem {len(self)} tarefa(s)")
+    
+    def __len__(self) -> int:
         current = self.first
         size = 0
         while current is not None:
             size += 1
             current = current.next
         return size
-    
-    def __len__(self) -> int:
-        return self.length()
+
+def menu(taskman: TaskManager):
+    try:
+        command = int(input("1) Adicionar Tarefa\n2) Procurar tarefa (requer nome)\n3) Remover Tarefa (requer índice)\n4) Quantidade de tarefas\n5) Mostrar tarefas\n6) Sair do programa\nDigite o que deseja fazer: "))
+
+        if command>6 or command<1:
+            raise ValueError
+        
+        match command:
+            case 1:
+                name = input("Digite o nome da tarefa:\n")
+                desc = input("Digite a descrição da tarefa:\n")
+                deadline = input("Digite o prazo da tarefa (no estilo DD/MM/AA ou DD/MM/AAAA):\n")
+                
+                taskman.append_task(name, desc, deadline)
+            case 2:
+                name = input("Digite o nome da tarefa:\n")
+                taskman.search_task(name)
+            case 3:
+                idx = int(input("Digite o índice da tarefa (a primeira tarefa tem índice zero):\n"))
+                taskman.remove_task(idx)
+            case 4:
+                taskman.length()
+            case 5:
+                taskman.show_tasks()
+            case 6:
+                raise KeyboardInterrupt
+            
+    except ValueError:
+        print("Digite um número válido!")
+
+    except IndexError:
+        print("Este índice não existe nas tarefas")
 
 if __name__=="__main__":
     taskman = TaskManager()
-
-    taskman.append_task("Atividade de CSD", "Entregar a atividade de CSD", "11/03/23")
-    taskman.append_task("Trabalho de ACE2", "Enviar o Trabalho de ACE2 no AVA", "04/03/23")
-    taskman.append_task("Reunião", "Reunião com o Prof. Adriano", "09/03/23")
-
-    taskman.show_tasks()
-
-    taskman.search_task("Trabalho de ACE2")
-
-    taskman.remove_task(0)
-
-    taskman.show_tasks()
+    
+    try:
+        while True:
+            menu(taskman)
+    except KeyboardInterrupt:
+        print("\nSaindo do programa...")
