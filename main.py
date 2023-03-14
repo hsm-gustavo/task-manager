8d0de6ea7905f80de47edbe5dbd874cf30967c75
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2023-03-14 13:01:33 -0300

    add 'edit_tasks' method, reduced unnecessary iterations, fixed empty string being appended to the list

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ class TaskManager:
         self.today = datetime.today().date().strftime("%d/%m/%y")
         self.first = None
         self.last = None
+        self.size = 0
 
     def append_task(self, name, desc, deadline):
         new_task = Task(name, desc, deadline)
         if self.is_empty():
             self.first = new_task
             self.last = new_task
+            self.size += 1
             return
         
         new_task.prev = self.last
         self.last.next = new_task
         self.last = new_task
+        self.size += 1
 
     def search_task(self, name):
         cur_task = self.first
@@ -56,26 +59,25 @@ Você está {abs(days_left)} dia(s) atrasado
         print("Tarefa não encontrada")
 
     def remove_task(self, index):
-        if len(self) == 0:
-            return print("\nVocê ainda não adicionou nenhuma tarefa\n")
-
-        elif index >= len(self) or index < 0:
+        if index >= len(self) or index < 0:
             raise IndexError("Index out of range")
         
-        if index == 0:
+        elif index == 0:
             self.first = self.first.next
             if self.first is not None:
                 self.first.prev = None
+            self.size -= 1
             return print("\n--------Tarefa removida--------\n")
         
         elif index == len(self)-1:
             self.last = self.last.prev
             if self.last is not None:
                 self.last.next = None
+            self.size -= 1
             return print("\n--------Tarefa removida--------\n")
         
-        current = self.first
-        cur_index = 0
+        current = self.first.next
+        cur_index = 1
 
         while cur_index != index:
             current = current.next
@@ -83,36 +85,81 @@ Você está {abs(days_left)} dia(s) atrasado
 
         current.prev.next = current.next
         current.next.prev = current.prev
+        self.size -= 1
 
         return print("\n--------Tarefa removida--------\n")
     
     def show_tasks(self):
         cur_task = self.first
-        print(f"\nVocê tem {len(self)} tarefa(s)\n")
+        self.length()
         while cur_task is not None:
             print(f"Nome: {cur_task.name}\nDescrição: {cur_task.desc}\nPrazo: {cur_task.deadline}")
             print("-------------------------------")
             cur_task = cur_task.next
 
+    def edit_task(self, index):
+        if index >= len(self) or index < 0:
+            raise IndexError("Index out of range")
+        
+        elif index == 0:
+            name = input("Digite o novo nome da tarefa:\n")
+            if name != "":
+                self.first.name = name
+            desc = input("Digite a nova descrição da tarefa:\n")
+            if desc != "":
+                self.first.desc = desc
+            deadline = input("Digite o novo prazo da tarefa (no estilo DD/MM/AA ou DD/MM/AAAA):\n")
+            if deadline != "":
+                self.first.deadline = deadline
+            return
+        
+        elif index == len(self)-1:
+            name = input("Digite o novo nome da tarefa:\n")
+            if name != "":
+                self.last.name = name
+            desc = input("Digite a nova descrição da tarefa:\n")
+            if desc != "":
+                self.last.desc = desc
+            deadline = input("Digite o novo prazo da tarefa (no estilo DD/MM/AA ou DD/MM/AAAA):\n")
+            if deadline != "":
+                self.last.deadline = deadline
+            return
+
+        current = self.first.next
+        cur_index = 1
+
+        while cur_index != index:
+            current = current.next
+            cur_index += 1
+        
+        name = input("Digite o novo nome da tarefa:\n")
+        if name != "":
+            current.name = name
+        desc = input("Digite a nova descrição da tarefa:\n")
+        if desc != "":
+            current.desc = desc
+        deadline = input("Digite o novo prazo da tarefa (no estilo DD/MM/AA ou DD/MM/AAAA):\n")
+        if deadline != "":
+            current.deadline = deadline
+
     def is_empty(self) -> bool:
         return self.first == None
     
     def length(self):
-        return print(f"Você tem {len(self)} tarefa(s)")
+        return print(f"\nVocê tem {len(self)} tarefa(s)\n")
     
     def __len__(self) -> int:
-        current = self.first
-        size = 0
-        while current is not None:
-            size += 1
-            current = current.next
-        return size
+        return self.size
+    
+    def __save_tasks(self, file:str=None):
+        with open(file, "w") as file:
+            pass
 
 def menu(taskman: TaskManager):
     try:
-        command = int(input("1) Adicionar Tarefa\n2) Procurar tarefa (requer nome)\n3) Remover Tarefa (requer índice)\n4) Quantidade de tarefas\n5) Listar tarefas\n6) Sair do programa\nDigite o que deseja fazer: "))
+        command = int(input("1) Adicionar Tarefa\n2) Procurar tarefa (requer nome)\n3) Remover Tarefa (requer índice)\n4) Quantidade de tarefas\n5) Listar tarefas\n6) Editar tarefa (requer índice)\n7) Sair do programa\nDigite o que deseja fazer: "))
 
-        if command>6 or command<1:
+        if command>7 or command<1:
             raise ValueError
         
         match command:
@@ -120,6 +167,9 @@ def menu(taskman: TaskManager):
                 name = input("Digite o nome da tarefa:\n")
                 desc = input("Digite a descrição da tarefa:\n")
                 deadline = input("Digite o prazo da tarefa (no estilo DD/MM/AA ou DD/MM/AAAA):\n")
+
+                if name == "" or desc == "" or deadline == "":
+                    return print("Você não pode deixar nenhum campo em branco!")
                 
                 taskman.append_task(name, desc, deadline)
             case 2:
@@ -133,13 +183,16 @@ def menu(taskman: TaskManager):
             case 5:
                 taskman.show_tasks()
             case 6:
+                idx = int(input("Digite o índice da tarefa (a primeira tarefa tem índice zero):\n"))
+                taskman.edit_task(idx)
+            case 7:
                 raise KeyboardInterrupt
             
     except ValueError:
         print("Digite um número válido!")
 
     except IndexError:
-        print("Este índice não existe nas tarefas")
+        print("Este índice não existe nas tarefas ou não foi digitado um índice válido")
 
 if __name__=="__main__":
     taskman = TaskManager()
@@ -149,3 +202,5 @@ if __name__=="__main__":
             menu(taskman)
     except KeyboardInterrupt:
         print("\nSaindo do programa...")
+
+# TODO: Add a way to store tasks in a file
