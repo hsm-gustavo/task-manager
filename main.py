ab85948be63f5d1c4e77e0de8787621598f3e7ea
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2023-04-05 21:16:57 -0300

    implemented priority (1 - highest/5 - lowest); implemented add_sorted and a sort method, both use the priority attribute

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
 class Task:
-    def __init__(self, name, desc, deadline) -> None:
+    def __init__(self, name, desc, deadline, priority: int) -> None:
         self.name = name
         self.desc = desc
         self.deadline = deadline
+        self.priority = priority
         self.next = None
         self.prev = None
 
@@ -16,18 +17,74 @@ class TaskManager:
         self.size = 0
         self.__load_tasks()
 
-    def append_task(self, name, desc, deadline):
-        new_task = Task(name, desc, deadline)
+    def append_task(self, new_task: Task):
+        new_task.prev = self.last
+        self.last.next = new_task
+        self.last = new_task
+        self.size += 1
+
+    def add_sorted(self, name, desc, deadline, priority: int = 0):
+        new_task = Task(name, desc, deadline, priority)
+
         if self.is_empty():
             self.first = new_task
             self.last = new_task
             self.size += 1
             return
         
-        new_task.prev = self.last
-        self.last.next = new_task
-        self.last = new_task
-        self.size += 1
+        elif new_task.priority < self.first.priority:
+            new_task.next = self.first
+            self.first.prev = new_task
+            self.first = new_task
+            self.size += 1
+            return
+        
+        elif new_task.priority >= self.last.priority:
+            self.append_task(new_task)
+            return
+        
+        current = self.first.next
+        while current is not None:
+            if new_task.priority < current.priority:
+                new_task.next = current
+                new_task.prev = current.prev
+                if current.prev is not None:
+                    current.prev.next = new_task
+                current.prev = new_task
+                self.size += 1
+                return
+            elif new_task.priority == current.priority:
+                new_task.next = current.next
+                new_task.prev = current
+                if current.next is not None:
+                    current.next.prev = new_task
+                current.next = new_task
+                self.size += 1
+                return
+            current = current.next
+
+    def sort(self):
+        if self.size < 2:
+            return
+
+        for i in range(self.size):
+            node = self.first
+            for j in range(self.size - 1 - i):
+                if node.priority > node.next.priority:
+                    next_node = node.next
+                    node.next = next_node.next
+                    if next_node.next is not None:
+                        next_node.next.prev = node
+                    next_node.prev = node.prev
+                    if node.prev is not None:
+                        node.prev.next = next_node
+                    else:
+                        self.first = next_node
+                    node.prev = next_node
+                    next_node.next = node
+                else:
+                    node = node.next
+
 
     def search_task(self, name):
         cur_task = self.first
@@ -42,12 +99,14 @@ class TaskManager:
                 s = str()
                 if days_left >= 0:
                     s = f'''-------Tarefa encontrada-------
+Prioridade: {cur_task.priority}
 Descrição: {cur_task.desc}
 Prazo: {cur_task.deadline}
 Você tem {days_left} dia(s) restante(s)
 '''
                 else:
                     s = f'''-------Tarefa encontrada-------
+Prioridade: {cur_task.priority}
 Descrição: {cur_task.desc}
 Prazo: {cur_task.deadline}
 Você está {abs(days_left)} dia(s) atrasado
@@ -94,7 +153,7 @@ Você está {abs(days_left)} dia(s) atrasado
         cur_task = self.first
         self.length()
         while cur_task is not None:
-            print(f"Nome: {cur_task.name}\nDescrição: {cur_task.desc}\nPrazo: {cur_task.deadline}")
+            print(f"Prioridade: {cur_task.priority}\nNome: {cur_task.name}\nDescrição: {cur_task.desc}\nPrazo: {cur_task.deadline}")
             print("-------------------------------")
             cur_task = cur_task.next
 
@@ -103,6 +162,9 @@ Você está {abs(days_left)} dia(s) atrasado
             raise IndexError("Index out of range")
         
         elif index == 0:
+            priority = input("Digite a nova prioridade da tarefa:\n")
+            if priority != "":
+                self.first.priority = int(priority)
             name = input("Digite o novo nome da tarefa:\n")
             if name != "":
                 self.first.name = name
@@ -115,6 +177,9 @@ Você está {abs(days_left)} dia(s) atrasado
             return
         
         elif index == len(self)-1:
+            priority = input("Digite a nova prioridade da tarefa:\n")
+            if priority != "":
+                self.last.priority = int(priority)
             name = input("Digite o novo nome da tarefa:\n")
             if name != "":
                 self.last.name = name
@@ -133,6 +198,9 @@ Você está {abs(days_left)} dia(s) atrasado
             current = current.next
             cur_index += 1
         
+        priority = input("Digite a nova prioridade da tarefa:\n")
+        if priority != "":
+            current.priority = int(priority)
         name = input("Digite o novo nome da tarefa:\n")
         if name != "":
             current.name = name
@@ -156,7 +224,7 @@ Você está {abs(days_left)} dia(s) atrasado
         with open("tasks.txt", "w") as file:
             current = self.first
             while current is not None:
-                file.write(f"{current.name}\n{current.desc}\n{current.deadline}")
+                file.write(f"{current.priority}\n{current.name}\n{current.desc}\n{current.deadline}")
                 file.write("\n")
                 current = current.next
     
@@ -164,13 +232,15 @@ Você está {abs(days_left)} dia(s) atrasado
         try:
             with open("tasks.txt", "r") as file:
                 lines = file.readlines()
-                for i in range(0, len(lines), 3):
-                    self.append_task(lines[i].strip(), lines[i+1].strip(), lines[i+2].strip())
-
+                for i in range(0, len(lines), 4):
+                    priority = int(lines[i].strip())
+                    name = lines[i+1].strip()
+                    desc = lines[i+2].strip()
+                    deadline = lines[i+3].strip()
+                    self.add_sorted(name, desc, deadline, priority)
         except FileNotFoundError:
-            pass
-            
-
+            pass   
+    
 def menu(taskman: TaskManager):
     try:
         command = int(input("1) Adicionar Tarefa\n2) Procurar tarefa (requer nome)\n3) Remover Tarefa (requer índice)\n4) Quantidade de tarefas\n5) Listar tarefas\n6) Editar tarefa (requer índice)\n7) Sair do programa\nDigite o que deseja fazer: "))
@@ -183,11 +253,15 @@ def menu(taskman: TaskManager):
                 name = input("Digite o nome da tarefa:\n")
                 desc = input("Digite a descrição da tarefa:\n")
                 deadline = input("Digite o prazo da tarefa (no estilo DD/MM/AA ou DD/MM/AAAA):\n")
+                priority = input("Digite a prioridade da tarefa (1 é a mais alta, 5 é a mais baixa):\n")
 
-                if name == "" or desc == "" or deadline == "":
+                if name == "" or desc == "" or deadline == "" or priority == "":
                     return print("Você não pode deixar nenhum campo em branco!")
                 
-                taskman.append_task(name, desc, deadline)
+                elif int(priority) > 5 or int(priority) < 1:
+                    return print("A prioridade deve ser um número entre 1 e 5!")
+                
+                taskman.add_sorted(name, desc, deadline, int(priority))
             case 2:
                 name = input("Digite o nome da tarefa:\n")
                 taskman.search_task(name)
@@ -201,6 +275,7 @@ def menu(taskman: TaskManager):
             case 6:
                 idx = int(input("Digite o índice da tarefa (a primeira tarefa tem índice zero):\n"))
                 taskman.edit_task(idx)
+                taskman.sort()
             case 7:
                 taskman.save_tasks()
                 raise KeyboardInterrupt
@@ -220,5 +295,3 @@ if __name__=="__main__":
     except KeyboardInterrupt:
         taskman.save_tasks()
         print("\nSaindo do programa...")
-
-# TODO: Add a way to store tasks in a file
